8afc638627a88353ddcf3e1a18ca8b43038a1a29
Allan Pimentel <allanpeter565@gmail.com>
2024-03-17 21:20:40 -0300

    routes working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,29 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home'
+import Plans from './components/Plans';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-
+ 
+    return(
+      <BrowserRouter>
+      <Routes>
+          <Route path='*' Component={Header} />
+          <Route path='*' Component={Footer} />
+          <Route path='/' Component={Home} />
+          <Route path='/' Component={Plans} />
+      </Routes>
+      </BrowserRouter>
+    )
   
-  return (
-    <div>
-      <Header />
-          <Home />
-      <Footer />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <Header />
+  //         <Home />
+  //     <Footer />
+  //   </div>
+  // );
 }
 
 export default App
